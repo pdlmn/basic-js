0dd781d98486cc0e1750976affadb03249f38966
pdlmn <djeinwings@gmail.com>
2022-04-30 18:17:22 +0200

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!")
+  const newArr = [...arr]
+
+  const double = which => (i, prevDeleted) => {
+    let item
+    which === 'next'
+      ? i >= newArr.length - 1
+        ? newArr.splice(i, 1)
+        : newArr.splice(i, 1, newArr[i + 1])
+      : i - 1 >= 0 && !prevDeleted
+        ? newArr.splice(i, 1, newArr[i - 1])
+        : newArr.splice(i, 1)
+    return newArr
+  }
+
+  const discard = which => (i, prevDeleted) => {
+    which === 'next'
+      ? newArr.splice(i, 2)
+      : i - 1 >= 0 && !prevDeleted
+        ? newArr.splice(i - 1, 2)
+        : newArr.splice(i, 1) 
+    return newArr
+  }
+  
+  const operations = {
+    '--double-next': double('next'),
+    '--double-prev': double('prev'),
+    '--discard-next': discard('next'),
+    '--discard-prev': discard('prev')
+  }
+
+  let deleted = []
+  for (let i = 0; i < newArr.length; i++) {
+    if (Object.keys(operations).includes(newArr[i])) {
+      const operation = newArr[i]
+      if (operation === '--discard-next') {
+        deleted.push(i)
+      }
+      let prevDeleted = deleted.includes(i) && operation.includes('prev')
+      operations[operation](i, prevDeleted)
+      i--
+    }
+  }
+  return newArr
 }
 
 module.exports = {
